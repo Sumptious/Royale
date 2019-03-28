7e71c278d3a076c93c8ca69343844972a38b6837
Sumptious <ruthny16@gmail.com>
2019-03-28 08:22:07 +0100

    push

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app =express();
-var path = ('path');
+var path = require ('path');
 var PORT = process.env.PORT || 4000;
 
 app.use(express.static (path.join(__dirname + '/dist')));
+
+app.get('/', function (req, res) {
+    res.render('index');
+  });
 
 app.listen(PORT, function() {
     console.log('app listening on port' + ' ' + PORT)
